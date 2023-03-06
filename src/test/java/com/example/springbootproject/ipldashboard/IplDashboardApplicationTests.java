cf9627518edde0b4e88957656c43b143a39f13c0
shekhar2702 <shekhar.prince1234@gmail.com>
2023-03-06 12:33:42 +0530

    Initial setup

---
+++ src/test/java/com/example/springbootproject/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootproject.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
