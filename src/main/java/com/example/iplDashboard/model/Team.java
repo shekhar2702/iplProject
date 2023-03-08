2565445f5a4d1a6ed5381ffbebe6f22ed4655863
shekhar2702 <shekhar.prince1234@gmail.com>
2023-03-08 11:44:12 +0530

    Fixed data values and created new entity called teams

---
+++ src/main/java/com/example/iplDashboard/model/Team.java
@@ -0,0 +1,57 @@
+package com.example.iplDashboard.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public long getTotalMatches() {
+        return totalMatches;
+    }
+
+    public void setTotalMatches(long totalMatches) {
+        this.totalMatches = totalMatches;
+    }
+
+    public long getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(long totalWins) {
+        this.totalWins = totalWins;
+    }
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", teamName='" + teamName + '\'' +
+                ", totalMatches=" + totalMatches +
+                ", totalWins=" + totalWins +
+                '}';
+    }
+}
