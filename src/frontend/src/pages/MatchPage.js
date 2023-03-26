31074b8301b95dc9fc529c64a4c5cfad693fc7d3
shekhar2702 <shekhar.prince1234@gmail.com>
2023-03-26 21:19:04 +0530

    Built initial match page with loading of match data from match endpoint

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,10 +1,27 @@
-import {React} from "react";
+import {React,useState,useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {MatchDetailCard} from "../components/MatchDetailCard";
 
 export const MatchPage = () => {
-    return (
+    const [matches,setMatches] = useState();
+    const {teamName,year} = useParams();
+    useEffect(()=>{
+        const getMatches = async() => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
+            // console.log(data);
+        }
+        getMatches();
+    },[])
+    return matches ? (
         <div className="MatchPage">
             <h1>Match Page.</h1>
+            <h1 className="page-heading">{teamName} matches in {year}</h1>
+            {matches.map(match => {
+                return <MatchDetailCard key={match.id} match={match} teamName={teamName} />
+            })}
         </div>
-    )
+    ) : null;
 }
 
