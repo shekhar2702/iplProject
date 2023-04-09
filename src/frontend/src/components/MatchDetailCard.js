efffca4961f6e212564fff641361ac01d41066c4
shekhar2702 <shekhar.prince1234@gmail.com>
2023-04-09 15:23:09 +0530

    Set up initial CSS and fixing class names

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -1,16 +1,18 @@
 import {React} from "react";
 import {Link} from "react-router-dom";
-
+import "./MatchDetailCard.scss";
 export const MatchDetailCard = ({match,teamName}) => {
     const opponentTeam = match.team1 === teamName ? match.team2 : match.team1;
     const opponentTeamRoute = `/teams/${opponentTeam}`;
     return (
         <div className="MatchDetailCard">
-            <h3>Latest Matches</h3>
-            <h1>VS <Link to={opponentTeamRoute}>{opponentTeam}</Link></h1>
-            <h2 className="match-date">{match.date}</h2>
-            <h3 className="match-venue">at {match.venue}</h3>
-            <h3 className="match-result">{match.winner} won by {match.resultMargin} {match.result} </h3>
+            <div>
+                <span className="vs">vs</span>
+                <h1><Link to={opponentTeamRoute}>{opponentTeam}</Link></h1>
+                <h2 className="match-date">{match.date}</h2>
+                <h3 className="match-venue">at {match.venue}</h3>
+                <h3 className="match-result">{match.winner} won bys {match.resultMargin} {match.result} </h3>
+            </div>
         </div>
     )
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,6 +2,7 @@ import {React, useState, useEffect} from "react";
 import {MatchDetailCard} from "../components/MatchDetailCard";
 import {MatchSmallCard} from "../components/MatchSmallCard";
 import {useParams} from "react-router-dom";
+import './TeamPage.scss';
 
 export const TeamPage = () => {
 
@@ -19,11 +20,17 @@ export const TeamPage = () => {
     },[teamName])
 
     return team ? (
-        <div className="TeamPage">
-        <h1>{team.teamName}</h1>
+   <div className="TeamPage">
+        <div className="team-name-section"><h1 className="team-name">{team.teamName}</h1></div>
+        <div className="win-loss-section">Wins/Losses</div>
+       <div className="match-detail-section">
         <MatchDetailCard match = {team.matches[0]}
                          teamName = {team.teamName}/>
-            {team.matches.slice(1).map(match => <MatchSmallCard key={match.id} match = {match} teamName = {teamName} />)}
+       </div>
+       {team.matches.slice(1).map(match => <MatchSmallCard key={match.id} match = {match} teamName = {teamName} />)}
+       <div>
+           <a href="#">More</a>
+       </div>
     </div>
     )
         : null;
